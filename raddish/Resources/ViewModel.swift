17c94ce4dc47f606cc95ae3b8eb4938395e8ff70
Ankita Kundu <ankitaku@ww109-a3.andrew.cmu.edu>
2021-12-02 15:22:44 -0500

    Adding in a working version

--- raddish/Resources/ViewModel.swift
+++ raddish/Resources/ViewModel.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Mira Mookerjee on 10/24/21.
 //
-
 import Foundation
 import Photos
 import SwiftUI
@@ -102,6 +101,7 @@ func populateRecipes() {
           }
     
     func createMealIngRecipe () {
+        // PROF H LOOK HERE!!!!!!!
         self.recipesIng.removeAll()
         for meal in self.mealIngredients {
             let recipe = meal.strMeal
@@ -179,6 +179,7 @@ func populateRecipes() {
     }
     
     func retrieveMealswithIng (ingredient: String){
+      //PROF H LOOK HERE!!!!!
         let basic_url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient
         
         let url = basic_url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "Recipe Not Available"
@@ -221,7 +222,7 @@ func populateRecipes() {
   
   func daysBetween(start: Date, end: Date) -> Int {
     return Calendar.current.dateComponents([.day], from: start, to: end).day! + 1
- 
+  }
 
   func savePantryItem(name: String?, expiration: Date?, date: Date?) {
     // create a new Pantry Item object
@@ -321,6 +322,7 @@ func populateRecipes() {
     }
     pantry.remove(atOffsets: atOffsets)
   }
+  
   func saveMealItem(day_of_week: String?, name: String?, imageURL: String?, instructions: String?,
                     ingredient1: String?, ingredient2: String?, ingredient3: String?,
                     ingredient4: String?, ingredient5: String?, ingredient6: String?,
@@ -363,14 +365,6 @@ func populateRecipes() {
         newMealItem.imageURL = nil
       }
     }
-    
-    //Compare all the ingredients in the Pantry with the
-    //ingredients required for this meal
-        
-    let recipe = createRecipe(recipe: newMealItem.name)
-    for pantryItem in self.pantry {
-            
-        }
      
     if let instructionsTemp = instructions {
       if instructionsTemp != "" {
@@ -870,5 +864,5 @@ func populateRecipes() {
       }
 
     meals.remove(at: meals.firstIndex(of: mealItem) ?? -1)
-  }   
+  }
 }
